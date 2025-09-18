ee0ecdd77d0d912f7ab566064c48ae005d0c7ae0
RxTavares <rafatcb0133@gmail.com>
2025-09-17 22:08:47 -0300

    Update Ex_While.c

--- Exercicíos e Projetos em linguagem C/Estrutura de repetição/Ex_While.c
+++ Exercicíos e Projetos em linguagem C/Estrutura de repetição/Ex_While.c
@@ -1,3 +1,8 @@
+
+/* Leia uma quantidade indeterminada de duplas de valores inteiros X e Y. Escreva para cada X e Y uma
+mensagem que indique se estes valores foram digitados em ordem crescente ou decrescente. O
+programa deve finalizar quando forem digitados dois valores iguais*/
+
 #include <stdio.h>
 
 int main(){
