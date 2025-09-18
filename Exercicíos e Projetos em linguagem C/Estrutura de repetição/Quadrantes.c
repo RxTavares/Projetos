f48af9f18c79356c3690e568ee1748c06a8fbf87
RxTavares <rafatcb0133@gmail.com>
2025-09-17 22:05:53 -0300

    Update Quadrantes.c

--- Exercicíos e Projetos em linguagem C/Estrutura de repetição/Quadrantes.c
+++ Exercicíos e Projetos em linguagem C/Estrutura de repetição/Quadrantes.c
@@ -1,4 +1,11 @@
+
+
+/*Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no
+sistema cartesiano. Para cada ponto escrever o quadrante a que ele pertence (Q1, Q2, Q3 ou Q4). O
+algoritmo será encerrado quando pelo menos uma de duas coordenadas for NULA 
+*/
 #include<stdio.h>
+
 
 int main(){
 
