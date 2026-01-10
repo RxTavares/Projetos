82ee939fbf52c5a5bea7b8e54adc2d7560f81eac
RxTavares <rafatcb0133@gmail.com>
2026-01-09 22:33:04 -0300

    Update Ex_Par e impar.c

--- Exercicios e Projetos em linguagem C/Estrutura de Repeticao/Ex_Par e impar.c
+++ Exercicios e Projetos em linguagem C/Estrutura de Repeticao/Ex_Par e impar.c
@@ -1,3 +1,10 @@
+/*Leia um valor inteiro N. Este valor será a quantidade de números inteiros que serão lidos em seguida.
+Para cada valor lido, mostre uma mensagem dizendo se este valor lido é PAR ou IMPAR, e também
+se é POSITIVO ou NEGATIVO. No caso do valor ser igual a zero (0), seu programa deverá imprimir
+apenas NULO. 
+*/
+
+
 #include<stdio.h>
 
 int main(){
@@ -24,6 +31,8 @@ int main(){
 
         }else if(x < 0  && x % 2 != 0){
             printf("%d Impar Negativo\n",x);
+        }else{
+        printf("Nulo\n");
         }
     }
     
