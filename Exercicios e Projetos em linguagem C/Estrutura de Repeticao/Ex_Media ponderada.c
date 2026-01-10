9efd8fd78bc060b1091011642e4f30d6f1aee532
RxTavares <rafatcb0133@gmail.com>
2026-01-09 23:52:15 -0300

    Update Ex_Media ponderada.c

--- Exercicios e Projetos em linguagem C/Estrutura de Repeticao/Ex_Media ponderada.c
+++ Exercicios e Projetos em linguagem C/Estrutura de Repeticao/Ex_Media ponderada.c
@@ -1,5 +1,34 @@
+/*Leia um valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de
+teste consiste de 3 valores reais, para os quais você deverá calcular e mostrar a média ponderada, sendo
+que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem peso 5. Vale lembrar
+que a média ponderada é a soma de todos os valores multiplicados pelo seu respectivo peso, dividida
+pela soma dos pesos.*/
+
 #include<stdio.h>
 
 int main(){
-    
+
+    int N,i;
+    float a,b,c,media;
+
+    printf("Numero de casos digitados: ");
+    scanf("%d",&N);
+
+    for(i = 0;i < N; i++){
+
+        printf("Digite tres numeros reais: \n");
+        scanf("%f",&a); // peso 2
+        scanf("%f",&b); // peso 3
+        scanf("%f",&c); // peso 5
+
+        media = ((a*2) + (b*3) + (c*5)) / 10;
+
+        printf("Media = %.1f\n",media);
+    }
+
+    printf("Fim dos calculos");
+
+
+
+return 0;
 }
