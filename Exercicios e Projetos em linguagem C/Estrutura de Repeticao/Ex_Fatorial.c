790bbfd4f81d6ee90e9346364da061396a3f429f
RxTavares <rafatcb0133@gmail.com>
2026-01-11 14:27:46 -0300

    Update Ex_Fatorial.c

---
+++ Exercicios e Projetos em linguagem C/Estrutura de Repeticao/Ex_Fatorial.c
@@ -0,0 +1,24 @@
+/*Fazer um programa para ler um número natural N (valor máximo: 15), e depois calcular e mostrar o
+fatorial de N. */
+
+
+#include<stdio.h>
+
+int main(){
+
+    int N,i;
+    long long fatorial = 1;
+
+
+    printf("Digite o valor de N: ");
+    scanf("%d",&N);
+
+    for ( i = 1; i <= N; i++)
+    {
+        fatorial *= 1;
+    }
+
+    printf("Fatorial de %d = %lld\n", N, fatorial);
+    return 0;
+}
+
