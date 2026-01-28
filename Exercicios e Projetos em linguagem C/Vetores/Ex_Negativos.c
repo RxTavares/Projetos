fdfc0a7c21554302cf5f2e54d62efcfb28222843
RxTavares <rafatcb0133@gmail.com>
2026-01-28 20:23:24 -0300

    Update Ex_Negativos.c

--- Exercicios e Projetos em linguagem C/Vetores/Ex_Negativos.c
+++ Exercicios e Projetos em linguagem C/Vetores/Ex_Negativos.c
@@ -1,4 +1,9 @@
+
+/*Problema "negativos"
+Faça um programa que leia um número inteiro positivo N (máximo = 10) e depois N números inteiros
+e armazene-os em um vetor. Em seguida, mostrar na tela todos os números negativos lidos. */
 #include<stdio.h>
+
 int main(){
 
 int v[10];
