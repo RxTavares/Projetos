e3a6decc730b10127ed8f6cd4bc7d0895c61928c
RxTavares <rafatcb0133@gmail.com>
2026-02-06 13:05:18 -0300

    Create Ex_Num Pares.c

---
+++ Exercicios e Projetos em linguagem C/Vetores/Ex_Num Pares.c
@@ -0,0 +1,36 @@
+/*Faça um programa que leia N números inteiros e armazene-os em um vetor. Em seguida, mostre na
+tela todos os números pares, e também a quantidade de números pares*/
+
+#include<stdio.h>
+
+int main(){
+
+    int N,i,qnt_pares;
+    int vet[100];
+
+    printf("Quantos numeros serao digitados ? \n");
+    scanf("%d",&N);
+
+    for ( i = 0; i < N; i++){
+
+        printf("Digite um numero: ");
+        scanf("%d",&vet[i]);
+    }
+    
+qnt_pares = 0;
+
+printf("Numeros pares: ");
+
+    for ( i = 0; i < N ; i++){
+        
+        if(vet[i] % 2 == 0){
+            printf("%d  ",vet[i]);
+
+            qnt_pares = qnt_pares + 1;
+        }
+    }
+    
+    printf("\n Quantidade de Pares = %d",qnt_pares);
+
+return 0;
+}
