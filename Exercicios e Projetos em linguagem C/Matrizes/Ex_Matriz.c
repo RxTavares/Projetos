9e8bd8644cf1aa271f0e2c68d14bdfe45c4bddd2
RxTavares <rafatcb0133@gmail.com>
2026-08-13 10:31:13 -0300

    Create Ex_Matriz.c

---
+++ Exercicios e Projetos em linguagem C/Matrizes/Ex_Matriz.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main (){
+
+int mat[3][3];
+
+//Elementos da matriz
+for  ( int i = 0; i < 3; i++)
+{
+    for( int j = 0; j < 3;j++ )
+    {
+        printf("\n Elemento = [%d][%d] -> ",i,j);
+        scanf("%d",&mat[i][j]);
+    }
+}
+
+//Imprimindo a matriz
+
+for(int i = 0; i < 3; i++)
+{
+    for( int j=0;j<3;j++)
+    {
+        printf("%d",mat [i] [j] );
+    }
+    printf("\n");
+}
+
+}
