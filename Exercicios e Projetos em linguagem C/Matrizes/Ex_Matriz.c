ba8993d6be1c22466dea945d79dee0d4ad23d2c1
RxTavares <rafatcb0133@gmail.com>
2026-08-13 23:35:30 -0300

    Update Ex_Matriz.c

--- Exercicios e Projetos em linguagem C/Matrizes/Ex_Matriz.c
+++ Exercicios e Projetos em linguagem C/Matrizes/Ex_Matriz.c
@@ -9,8 +9,7 @@ for  ( int i = 0; i < 3; i++)
 {
     for( int j = 0; j < 3;j++ )
     {
-        printf("\n Elemento = [%d][%d] -> ",i,j);
-        scanf("%d",&mat[i][j]);
+        mat [i][j] = i+j;
     }
 }
 
